2630661c41f5fbcb3378ae1dda196d23eb19a0bc
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-09 19:37:38 +0200

    using .rstrip(), .lstrip(), and .strip() to remove whitespace from a string, and using escape characters to include special characters in a string.

--- Python_Chapters/Chapter 2/Classwork/practice.py
+++ Python_Chapters/Chapter 2/Classwork/practice.py
@@ -1,1 +1,7 @@
+favorite_color = "blue and green   "
+print(favorite_color.rstrip()) #removes trailing whitespace
+print(favorite_color.lstrip()) #removes leading whitespace
+print(favorite_color.strip()) #removes both leading and trailing whitespace
 
+message = "One of python's strengths is its diverse community."
+print(message)
