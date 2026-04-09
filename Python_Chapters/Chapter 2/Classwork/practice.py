b88d5d592772efce07a022f8b29c5ddd723266f9
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-09 21:25:36 +0200

    practice on quotation marks

--- Python_Chapters/Chapter 2/Classwork/practice.py
+++ Python_Chapters/Chapter 2/Classwork/practice.py
@@ -1,7 +1,2 @@
-favorite_color = "blue and green   "
-print(favorite_color.rstrip()) #removes trailing whitespace
-print(favorite_color.lstrip()) #removes leading whitespace
-print(favorite_color.strip()) #removes both leading and trailing whitespace
-
-message = "One of python's strengths is its diverse community."
-print(message)
+famous_quote = 'The only way to do great work is to love what you do. - "Steve Jobs"'
+print(famous_quote)
