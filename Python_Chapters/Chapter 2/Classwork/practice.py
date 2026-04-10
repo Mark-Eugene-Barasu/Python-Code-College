7fa7e216e3f5e209476e513c9f19b54a7ae071ce
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-10 19:52:00 +0200

    manipulating strings with index[]

--- Python_Chapters/Chapter 2/Classwork/practice.py
+++ Python_Chapters/Chapter 2/Classwork/practice.py
@@ -1,2 +1,21 @@
-famous_quote = 'The only way to do great work is to love what you do. - "Steve Jobs"'
-print(famous_quote)
+
+# checking out the index[] of a string
+word = "Python"
+print(word[0])  # Output: P
+print(word[5])  # Output: n
+
+# slicing a string
+print(word[1:4])  # Output: yth
+print(word[:3])   # Output: Pyt
+print(word[3:6])  # Output: hon
+
+# indexing
+print(word[-1])  # Output: n
+print(word[-6])  # Output: P
+
+# using the len() function to find the length of a string
+print(f"Length of word: {len(word)}")  # Output: 6
+
+# new line and tab
+print("Hello\nWorld")  # Output: Hello (new line) World
+print("Hello\tWorld")  # Output: Hello (tab) World
