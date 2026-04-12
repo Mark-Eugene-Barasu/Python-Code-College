e118a8635ba62adca82e701f59bec767722e42c9
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-12 08:15:24 +0200

    created P30 for tryout and did P29 try-out activities

---
+++ Python_Chapters/Chapter 2/Classwork/P29/practice.py
@@ -0,0 +1,9 @@
+# 2.9 Number Eight
+print(5 + 3)  # Output: 8
+print(10 - 2)  # Output: 8
+print(4 * 2)  # Output: 8
+print(16 / 2)  # Output: 8.0
+
+# 2.10 Favorite Number
+favorite_number = 7
+print(f"My favorite number is {favorite_number}.")  # Output: My favorite number is 7.
---
+++ Python_Chapters/Chapter 2/Classwork/P30/comment.py
@@ -0,0 +1,5 @@
+# is a a single line comment
+"""
+is a multi-line comment
+This is useful for longer explanations or documentation.
+"""
