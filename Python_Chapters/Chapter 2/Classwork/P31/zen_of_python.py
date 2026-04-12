a4fac4f962a46c0c24a970529b4b09c5ef64c83d
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-12 09:45:46 +0200

    made the comments in P31's file more descriptive

--- Python_Chapters/Chapter 2/Classwork/P31/zen_of_python.py
+++ Python_Chapters/Chapter 2/Classwork/P31/zen_of_python.py
@@ -3,3 +3,28 @@
 
 import this
 
+"""
+running this code will print the Zen of Python, which is a collection of guiding principles for writing computer programs in the Python language. It was written by Tim Peters and serves as a set of aphorisms that capture the philosophy of Python programming. The Zen of Python emphasizes simplicity, readability, and the importance of writing code that is easy to understand and maintain. It includes principles such as 
+
+1. Beautiful is better than ugly.
+2. Explicit is better than implicit.
+3. Simple is better than complex.
+4. Complex is better than complicated.
+5. Flat is better than nested.
+6. Sparse is better than dense.
+7. Readability counts.
+8. Special cases aren't special enough to break the rules.
+9. Although practicality beats purity.
+10. Errors should never pass silently.
+11. Unless explicitly silenced.
+12. In the face of ambiguity, refuse the temptation to guess.
+13. There should be one-- and preferably only one --obvious way to do it.
+14. Although that way may not be obvious at first unless you're Dutch.
+15. Now is better than never.
+16. Although never is often better than *right* now.
+17. If the implementation is hard to explain, it's a bad idea.
+18. If the implementation is easy to explain, it may be a good idea.
+19. Namespaces are one honking great idea -- let's do more of those!
+
+By following these principles, Python developers can create code that is not only functional but also elegant and easy to read.                                      
+"""
