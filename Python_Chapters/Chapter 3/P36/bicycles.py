b0f1ca578ff670c0dc9762b808481f347f9c7e62
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-14 20:25:38 +0200

    completed activities on page 36

--- Python_Chapters/Chapter 3/P36/bicycles.py
+++ Python_Chapters/Chapter 3/P36/bicycles.py
@@ -1,8 +1,27 @@
-bicycles = ["trek", "cannondale", "redline", "specialized"]
-print(type(bicycles), "\n")
-print(bicycles, "\n")
+# 3.1 Names
+names = ["Alfred", "Desmond", "Kofi", "Samuel", "William", "Kwesi"]
+print(names[0])
+print(names[1])
+print(names[2])
+print(names[3])
+print(names[4])
+print(names[5])
 
-for x in bicycles:
-    if bicycles.index(x) == 2:
-        continue
-    print(f"{x} is at index number: {bicycles.index(x)}", "\n")
+
+# 3.2 Greeting
+print(f"My junior brother is called {names[0]} and I love him so much. {"\n"}")
+print(f"{names[1]} created his own nail saloon shop in East London {"\n"}")
+print(
+    f"I just hope {names[2]} is doing okay, since it's been a long time. {"\n"}")
+print(f"Did you know that {names[3]} is a very good panel-beater? {"\n"}")
+print(f"{names[4]} from China won the 2022 Hackathon in the U.S.A. {"\n"}")
+print(f"Of course, {names[-1]} went back to university. {"\n"}")
+print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+
+# 3.3 Your Own List:
+transports = ["airplane", "car", "bicycle", "ship", "motorcycle"]
+print(f"I once used an {transports[0]} to travel from Kigali to Lusaka {"\n"}")
+print(f"According to society, it is an achievement to own a {transports[1]} {"\n"}")
+print(f"So mommy decided to sell my {transports[2]} without my consent {"\n"}")
+print(f"{transports[3]} are a very scary mode of transport {"\n"}")
+print(f"Did you know {transports[-1]} can fly in GTA? {"\n"}")
