a1ab5457d8d1fa8827ef53a0f29b4b67488ee6ec
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-15 20:17:41 +0200

    practicing activities

--- Python_Chapters/Chapter 4/Class-work/practice.py
+++ Python_Chapters/Chapter 4/Class-work/practice.py
@@ -1,4 +1,15 @@
-colors = ["blue", "yellow", "green", "black"]
+# fruits = ["apple", "banana", "cherry", "date"]
 
-for color in colors:
-    print(color)
+# for fruit in fruits:
+#     length = len(fruit)
+#     print(f"{fruit.title()} has {length} letters.")
+
+# Using range() function
+# even_numbers = list(range(0, 11, 2))  # parameters => (start, stop, step)
+# print(even_numbers)
+
+# squares = []
+# for number in range(1, 7):
+#     square = number ** 2
+#     squares.append(square)
+# print(squares)
