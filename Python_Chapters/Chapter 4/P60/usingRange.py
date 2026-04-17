96297651c6cd364e7d413aae203e3cc05ccaf040
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-17 17:13:46 +0200

    completed assignment on page 60 about lists

--- Python_Chapters/Chapter 4/P60/usingRange.py
+++ Python_Chapters/Chapter 4/P60/usingRange.py
@@ -4,5 +4,14 @@ for x in range(1, 21):
 
 # 4.4 Thirty:
 for y in range(0, 31):
-    print(y) 
+    print(y)
 
+# 4.5 Summing a Million
+numbers = list(range(1, 1000001))
+
+print(f"Minimum: {min(numbers)}")
+print(f"Maximum: {max(numbers)}")
+print(f"Sum: {sum(numbers)}")
+
+# 4.6 Odd numbers
+odd_numbers = list(range(1, 21, 2))
