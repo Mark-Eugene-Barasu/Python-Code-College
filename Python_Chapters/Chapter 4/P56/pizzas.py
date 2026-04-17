2964e9c4bb979f35e22090815ff954d751af334b
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-04-17 13:52:22 +0200

    assignment

---
+++ Python_Chapters/Chapter 4/P56/pizzas.py
@@ -0,0 +1,16 @@
+# 4.1 Pizzas
+pizzaNames = ["Debonairs", "Panarottis", "Roman's", "Eddie's", "Pizza Perfect"]
+for pizza in pizzaNames:
+    print(f"My number {int(pizzaNames.index(pizza)) + 1} best pizza is {pizza} Pizza", "\n")
+
+print(f"I love pizza so much that it makes me dance \"azonto\".")
+
+
+
+# 4.2 Animals (I chose dinosaurs)
+animals = ["Spinosaurus", "Chicken", "Velociraptor", "Dilophosaurus", "Brontosaurus"]
+for animal in animals:
+    print(f"I just realized that {animal} is/was a dinosaur? \n")
+
+print(f"All these dinosaurs will be great to have in an adventurous movie")
+
---
+++ Python_Chapters/Chapter 4/P60/usingRange.py
@@ -0,0 +1,8 @@
+# 4.3 Counting to twenty:
+for x in range(1, 21):
+    print(x)
+
+# 4.4 Thirty:
+for y in range(0, 31):
+    print(y) 
+
