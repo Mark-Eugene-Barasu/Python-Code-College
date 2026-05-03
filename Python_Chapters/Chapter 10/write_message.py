28962695c6b10eb511a3b618aee7d58fa0d21cad
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-05-03 13:12:08 +0200

    100% done with Chapter 10

--- Python_Chapters/Chapter 10/write_message.py
+++ Python_Chapters/Chapter 10/write_message.py
@@ -4,7 +4,7 @@ from pathlib import Path
 contents = "I love playing.\n"
 contents += "I love creating new games.\n"
 contents += "I also love working with data.\n"
-contents += "I love eating SWEETS!!!!!!.\n"
+contents += "I love computers\n"
 
 path = Path(r'C:\Users\mark0\OneDrive\Desktop\Code College\Python\Python_Chapters\Chapter 10\messages.txt')
 path.write_text(contents)
