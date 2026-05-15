e3bc5ff1aed997b9899425a4dba53ad9357b6c8d
Eugene Mark Korku Barasu <mark0630227248@gmail.com>
2026-05-15 04:56:16 +0200

    practice chapter 15

--- Python_Tutorials/Tutorial 2/Chapter 15/practice.py
+++ Python_Tutorials/Tutorial 2/Chapter 15/practice.py
@@ -3,7 +3,8 @@ from matplotlib import pyplot as plt
 input_values = [1, 2, 3, 4, 5]
 squares = [1, 4, 9, 16, 25]
 
-plt.plot(input_values, squares)
+plt.plot(input_values, squares, linewidth=3)  # Line graph
+plt.scatter(input_values, squares)
 plt.title("Square Numbers", fontsize=24)
 plt.xlabel("Value (x axis)", fontsize=14)  
 plt.ylabel("Square of Value (y axis)", fontsize=14)
